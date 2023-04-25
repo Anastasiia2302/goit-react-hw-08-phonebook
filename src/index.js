6949a89afc97c5504132ea35689da65658ac79db
Anastasiia Herusova <nastia.herusova@gmail.com>
2023-04-25 10:17:56 +0300

    update

--- src/index.js
+++ src/index.js
@@ -9,13 +9,14 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { persistor, store } from 'redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/goit-react-hw-08-phonebook/">
+        <BrowserRouter >
           <App />
         </BrowserRouter>
       </PersistGate>
     </Provider>
-  </React.StrictMode>
+  
 );
+// basename="/goit-react-hw-08-phonebook/"
--- src/pages/contacts .js
+++ src/pages/contacts .js
@@ -2,12 +2,13 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Helmet } from 'react-helmet';
 import Phonebook from 'components/Phonebook/Phonebook';
-import { getIsLoading } from 'redux/contacts/selectors';
+
 import { fetchContacts } from 'redux/contacts/operations';
+import { selectIsLoading } from 'redux/contacts/selectors';
 
 export default function Contacts() {
   const dispatch = useDispatch();
-  const isLoading = useSelector(getIsLoading);
+  const isLoading = useSelector(selectIsLoading);
 
   useEffect(() => {
     dispatch(fetchContacts());
