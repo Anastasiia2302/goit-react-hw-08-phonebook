6dc5edbcfd335031b97ee61103e4cbcc09338b89
Anastasiia Herusova <nastia.herusova@gmail.com>
2023-04-26 16:47:27 +0300

    styled-img

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,12 @@
 import { Container } from 'components/PhoneBook.styled';
+import 'animate.css';
 
 export default function home() {
   return (
     <Container>
       
       <h1>Phonebook</h1>
-      <img src={require('../Image/4298345.png')} alt="phone" width='400px' />
+      <img src={require('../Image/4298345.png')} alt="phone" width='400px' className='animate__animated animate__pulse animate__delay-2s animate__infinite' />
     </Container>
     
   );
